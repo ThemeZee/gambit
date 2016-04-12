0a22007c419424121f1957da3a6015cbcf7f813f
Thomas W <netzberufler@googlemail.com>
2016-04-12 11:00:28 +0200

    updated flexslider #2

--- inc/slider.php
+++ inc/slider.php
@@ -26,7 +26,7 @@ function gambit_slider_scripts() {
 		wp_enqueue_style( 'gambit-flexslider', get_template_directory_uri() . '/css/flexslider.css' );
 
 		// FlexSlider JS
-		wp_enqueue_script( 'gambit-flexslider', get_template_directory_uri() .'/js/jquery.flexslider-min.js', array('jquery'), '2.5.0' );
+		wp_enqueue_script( 'gambit-flexslider', get_template_directory_uri() .'/js/jquery.flexslider-min.js', array('jquery'), '2.6.0' );
 
 		// Register and enqueue slider.js
 		wp_enqueue_script( 'gambit-post-slider', get_template_directory_uri() .'/js/slider.js', array('gambit-flexslider') );
