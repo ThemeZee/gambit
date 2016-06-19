79f1549c016c7568ccc8742aceac076c13b36a93
Thomas W <netzberufler@googlemail.com>
2016-06-19 17:13:35 +0200

    improved accessibility of post navigation and pagination

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -274,7 +274,10 @@ function gambit_post_navigation() {
 
 	if ( true === $theme_options['post_navigation'] ) {
 
-		the_post_navigation( array( 'prev_text' => '&laquo; %title', 'next_text' => '%title &raquo;' ) );
+		the_post_navigation( array(
+			'prev_text' => '<span class="screen-reader-text">' . esc_html_x( 'Previous Post:', 'post navigation', 'gambit' ) . '</span>%title',
+			'next_text' => '<span class="screen-reader-text">' . esc_html_x( 'Next Post:', 'post navigation', 'gambit' ) . '</span>%title',
+		) );
 
 	}
 
@@ -334,8 +337,8 @@ function gambit_pagination() {
 		'format' => '?paged=%#%',
 		'current' => max( 1, get_query_var( 'paged' ) ),
 		'total' => $wp_query->max_num_pages,
-		'next_text' => '&raquo;',
-		'prev_text' => '&laquo',
+		'next_text' => '<span class="screen-reader-text">' . esc_html_x( 'Next Posts', 'pagination', 'gambit' ) . '</span>&raquo;',
+		'prev_text' => '&laquo<span class="screen-reader-text">' . esc_html_x( 'Previous Posts', 'pagination', 'gambit' ) . '</span>',
 		'add_args' => false,
 	) );
 
