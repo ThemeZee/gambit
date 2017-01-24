dd0755f34f2f378701685eaa9a554a8681e670f3
Lenovo-PC\thomasw <kontakt@themecoder.de>
2017-01-24 16:14:27 +0100

    removed empty sidebar messages

--- sidebar-small.php
+++ sidebar-small.php
@@ -13,16 +13,6 @@
 
 			dynamic_sidebar( 'sidebar-small' );
 
-			// Show hint where to add widgets.
-		else : ?>
-
-			<aside class="widget clearfix">
-				<div class="widget-header"><h3 class="widget-title"><?php esc_html_e( 'Small Sidebar', 'gambit' ); ?></h3></div>
-				<div class="textwidget">
-					<p><?php esc_html_e( 'Please go to Appearance &#8594; Widgets and add some widgets to your sidebar.', 'gambit' ); ?></p>
-				</div>
-			</aside>
-
-	<?php endif; ?>
+		endif; ?>
 
 	</section><!-- #secondary -->
