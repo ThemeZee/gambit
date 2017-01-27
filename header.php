bc34d97aaf38425438f4349c5d58d17c2a604e7f
Lenovo-PC\thomasw <kontakt@themecoder.de>
2017-01-27 13:53:51 +0100

    removed header bar wrapper markup

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
 
 		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'gambit' ); ?></a>
 
-		<div id="header-top" class="header-bar-wrap"><?php do_action( 'gambit_header_bar' ); ?></div>
+		<?php do_action( 'gambit_header_bar' ); ?>
 
 		<header id="masthead" class="site-header clearfix" role="banner">
 
