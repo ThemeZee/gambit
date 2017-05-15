4eb5ccebd116fd42d305bdb9808b71fb36befe28
Lenovo-PC\thomasw <kontakt@themecoder.de>
2017-05-15 15:21:29 +0200

    fixed small live preview issue in Customizer

--- inc/customizer/customizer.php
+++ inc/customizer/customizer.php
@@ -108,7 +108,7 @@ function gambit_customize_partial_blogdescription() {
  * Embed JS file to make Theme Customizer preview reload changes asynchronously.
  */
 function gambit_customize_preview_js() {
-	wp_enqueue_script( 'gambit-customizer-preview', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20170223', true );
+	wp_enqueue_script( 'gambit-customizer-preview', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20170515', true );
 }
 add_action( 'customize_preview_init', 'gambit_customize_preview_js' );
 
