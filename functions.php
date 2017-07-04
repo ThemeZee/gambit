f92ba42758ff2fc65a173dc9c909ad34570f3ce0
Lenovo-PC\thomasw <kontakt@themecoder.de>
2017-07-04 16:53:20 +0200

    added default titles for Magazine Widgets and adjusted description

--- functions.php
+++ functions.php
@@ -134,7 +134,7 @@ function gambit_widgets_init() {
 	register_sidebar( array(
 		'name' => esc_html__( 'Magazine Homepage', 'gambit' ),
 		'id' => 'magazine-homepage',
-		'description' => esc_html__( 'Appears on Magazine Homepage template only. You can use the Magazine Posts widgets here.', 'gambit' ),
+		'description' => esc_html__( 'Appears on blog index and Magazine Homepage template. You can use the Magazine widgets here.', 'gambit' ),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget' => '</div>',
 		'before_title' => '<div class="widget-header"><h3 class="widget-title">',
