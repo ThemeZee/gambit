7a302abe0e5e3d07efdf0db7e718beb40c441e62
Thomas W <kontakt@themecoder.de>
2018-04-13 19:03:01 +0200

    moved assets into new folder

--- inc/theme-info.php
+++ inc/theme-info.php
@@ -172,7 +172,7 @@ function gambit_theme_info_page_css( $hook ) {
 	}
 
 	// Embed theme info css style.
-	wp_enqueue_style( 'gambit-theme-info-css', get_template_directory_uri() . '/css/theme-info.css' );
+	wp_enqueue_style( 'gambit-theme-info-css', get_template_directory_uri() . '/assets/css/theme-info.css' );
 
 }
 add_action( 'admin_enqueue_scripts', 'gambit_theme_info_page_css' );
