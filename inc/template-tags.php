57d65d1da9826b3153b2e782ae1666263cbe6df0
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-05-03 18:07:46 +0200

    fixed typo in pagination function

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -328,7 +328,7 @@ if ( ! function_exists( 'gambit_pagination' ) ) :
 
 		the_posts_pagination( array(
 			'mid_size'  => 2,
-			'prev_text' => '&laquo<span class="screen-reader-text">' . esc_html_x( 'Previous Posts', 'pagination', 'gambit' ) . '</span>',
+			'prev_text' => '&laquo;<span class="screen-reader-text">' . esc_html_x( 'Previous Posts', 'pagination', 'gambit' ) . '</span>',
 			'next_text' => '<span class="screen-reader-text">' . esc_html_x( 'Next Posts', 'pagination', 'gambit' ) . '</span>&raquo;',
 		) );
 
